4b67373ccbaf646cbab03ae6ad352591545b7318
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-16 18:13:36 -0700

    11 files at root
    The 11 root files for this directory, in HTML, PNG, JS, XCF, ICO, and ICO_REMINDER formats.

---
+++ SNU_Committee/VarTestOriginal12.02.2019.js
@@ -0,0 +1,16 @@
+// Committee count
+console.log("Committee count: ");
+int x1 = 4; // Governing committees
+int x2 = 1; // Content committees
+int x3 = 2; // Adult content committees
+int x4 = 1; // Video committees
+int x5 = 2; // Maintenance committees
+int x6 = 1; // Document committees
+int x7 = 1; // Audio committees
+
+console.log(4 + 1 + 2 + 1 + 2 + 1 + 1);
+// Or (coming soon, when I can fix this JS)
+console.log("x1-x7 is not defined as the values have been commented out");
+console.log(x1 + x2 + x3 + x4 + x5 + x6 + x7);
+
+// This is the original source written during the day before I could fix the small error. Archived for historical purposes (Sean learns JavaScript)
